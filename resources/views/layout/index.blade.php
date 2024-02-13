81cd72a4cc087d13af47919f5464699fff87bca4
Dendrasaifulyandho <dendrasaifulyandho07@gmail.com>
2024-02-13 10:05:32 +0700

    commit1

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -9,7 +9,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
+
 {{-- navbar --}}
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Rentalin</a>
@@ -84,6 +84,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="{{ asset('js/scripts.js') }}"></script>
+
 
 </body>
 </html>
