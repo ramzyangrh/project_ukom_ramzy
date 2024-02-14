0557aca7d1e5a2bb575497aa3bd09e0868a287fb
M-Arsya <arsyayaeger@gmail.com>
2024-02-14 11:18:10 +0700

    up14feb

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardownController;
 use App\Http\Controllers\DashboardadmController;
-=======
 use App\Http\Controllers\LoginController;
->>>>>>> e42755fdd386f5d5288d3f0eeca16534bda2f995
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,12 +18,9 @@ use App\Http\Controllers\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 Route::get('/dashboardown', [DashboardownController::class, 'index']);
 // Route::get('/pmmobil', [pmmobilController::class, 'index'])->name('pmmobil.index');
 Route::get('/dashboardadm', [DashboardadmController::class, 'index']);
-=======
 Route::get('/dashboard', [DashboardownController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
 // Route::get('/pmmobil', [pmmobilController::class, 'index'])->name('pmmobil.index');
->>>>>>> e42755fdd386f5d5288d3f0eeca16534bda2f995
